e9065a8087ac0da5cfc034ddd51475392183bde6
Reut Vladislav <reut-2011@mail.ru>
2024-10-14 19:19:58 +0300

    feat: add dark and light theme, styles variables

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,34 @@ import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
 import "./App.scss";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  useEffect(() => {
+    // Устанавливаем data-theme на элемент
+    const personalCard = document.querySelector(".personal-card");
+    if (personalCard) {
+      personalCard.setAttribute("data-theme", isDarkMode ? "dark" : "light");
+    }
+  }, [isDarkMode]);
+
   return (
     <div className="personal-card">
+      <div className="wrapper">
+        <input
+          type="checkbox"
+          name="checkbox"
+          className="switch"
+          onChange={toggleTheme}
+          checked={isDarkMode}
+        />
+      </div>
+
       <Header />
       <Main />
       <Footer />
